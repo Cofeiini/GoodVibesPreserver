62a01fd03bddce6619bee968e48873e52a80d8ef
Cofeiini <cofeiini@gmail.com>
2023-08-18 00:03:36 +0300

    Update features
    * Add GitHub Response interface
    * Make various functions synchronous
    * Convert return values of various functions
    * Change how page loading is handled with buffers
    * Update custom HTML by removing deprecated elements

--- scripts/background/background.ts
+++ scripts/background/background.ts
@@ -19,6 +19,24 @@ interface filterResults
     blocked: boolean,
 }
 
+interface githubResponse {
+    "type": string,
+    "size": number,
+    "name": string,
+    "path": string,
+    "content": string,
+    "sha": string,
+    "url": string,
+    "git_url": string | null,
+    "html_url": string | null,
+    "download_url": string | null,
+    "_links": {
+        "git": string,
+        "html": string,
+        "self": string
+    }
+}
+
 let db : IDBDatabase;
 const dbRequest : IDBOpenDBRequest = window.indexedDB.open("filterDatabase",2);
 
@@ -36,80 +54,101 @@ dbRequest.onerror = (event) => {
     console.log((event.target as IDBRequest).error)
 }
 
-async function storeUrlData()
-{
-
-    const response = await fetch(filtersUrl,{
-        headers:{
+const storeUrlData = () => {
+    fetch(filtersUrl,{
+        headers: {
             Authorization:`<(￣︶￣)↗ token here`
         }
-    })
-    const responseJSON = await response.json();
-    const filtersBase64 : string = responseJSON.content;
-    const filtersString : string = atob(filtersBase64);
-    const IDBFilters : urlFilter[] = JSON.parse(filtersString);
-    const transaction : IDBTransaction = db.transaction(['filterList'],'readwrite');
-    const storeObject : IDBObjectStore = transaction.objectStore('filterList');
-    IDBFilters.forEach(filter =>{
-        storeObject.add(filter);
-    })
+    }).then(response => response.json())
+        .then((responseJSON: githubResponse) => {
+            const filtersBase64 : string = responseJSON.content;
+            const filtersString : string = atob(filtersBase64);
+            const IDBFilters : urlFilter[] = JSON.parse(filtersString);
+            const transaction : IDBTransaction = db.transaction(['filterList'],'readwrite');
+            const storeObject : IDBObjectStore = transaction.objectStore('filterList');
+            IDBFilters.forEach((filter: urlFilter) => {
+                storeObject.add(filter);
+            });
+        });
 }
 
 // URL blocking
+const isBlockedUrl = async (hostname: string, url: URL) : Promise<filterResults> => {
+    const transaction: IDBTransaction = db.transaction(['filterList'], 'readonly')
+    const storeObject: IDBObjectStore = transaction.objectStore('filterList')
+    const promise: Promise<filterResults> = new Promise((resolve, _) => {
+        const getDataRequest: IDBRequest = storeObject.getAll();
+        getDataRequest.onsuccess = (event: Event) => {
+            const data: urlFilter[] = (event.target as IDBRequest).result;
+            data.forEach((filter: urlFilter) => {
+                if (hostname.match(filter.pattern)) {
+                    resolve({
+                        "sitename": hostname,
+                        "tags": filter.tags,
+                        "blocked": true,
+                        "url": url
+                    });
+                }
+            });
 
-function isBlockedUrl(hostname : string,url : URL) : Promise<filterResults>
-{
-    return new Promise<filterResults>((resolve,reject) =>{
-        const transaction : IDBTransaction = db.transaction(['filterList'],'readonly')
-        const storeObject : IDBObjectStore = transaction.objectStore('filterList')
-        const getDataRequest : IDBRequest = storeObject.getAll()
-    
-        getDataRequest.onsuccess = (event) => {
-            const data : urlFilter[] = (event.target as IDBRequest).result;
-            data.forEach(filter => {
-                if(hostname.match(filter.pattern)) { resolve({"sitename":hostname,"tags":filter.tags,"blocked":true,"url":url}) }
-            })
-            resolve({"sitename":"","tags":"","blocked":false,"url":url});
+            resolve({
+                "sitename": "",
+                "tags": [""],
+                "blocked": false,
+                "url": url
+            });
         }
-    }) 
+    });
+
+    return await promise.then((results: filterResults) => {
+        return results;
+    });
 }
 
-let isFiltered : boolean;
-
-function handleRequest(details: _OnBeforeRequestDetails) : BlockingResponse | Promise<BlockingResponse> | void {
+const handleRequest = (details: _OnBeforeRequestDetails) : BlockingResponse | Promise<BlockingResponse> | void => {
     const filter: browser.webRequest.StreamFilter = browser.webRequest.filterResponseData(details.requestId);
-    const decoder: TextDecoder = new TextDecoder("utf-8");
     const encoder: TextEncoder = new TextEncoder();
 
-    filter.ondata = async(event: _StreamFilterOndataEvent) => {
-        const str: string = decoder.decode(event.data, { stream: true });
-        const url : URL = new URL(details.url)
-        console.log(url);
-        const ignoringFilter : boolean =  url.searchParams.get("skipfilter") ? true : false
-        console.log(ignoringFilter);
-        const isBlocked : filterResults = await isBlockedUrl(url.hostname,url);
-        if(isBlocked.blocked && !ignoringFilter) {
-            isBlocked.url.searchParams.set("skipfilter","true");
-            filter.write(encoder.encode(
-                `
-                <body style="background-color: rgb(145,26,26)">
-                <center>
-                <h1>The site ${isBlocked.sitename} is considered a dangerous website.</h1>
-                <h2>It is considered a site that contains: ${isBlocked.tags}</h2>
-                <a href="${isBlocked.url.href}" style="text-decoration: none; background-color: rgb(80,10,10); border-radius: 5px; color: black; padding: 5px">Proceed anyways</a>
-                </center>
-                </body>
-                `
-            ));
-            isFiltered = true;
-            return {};
-        } else {
-            filter.write(encoder.encode(str));
-        }
-        filter.disconnect();
+    const url : URL = new URL(details.url);
+    console.log(url);
+
+    let buffer : Uint8Array = new Uint8Array();
+    filter.ondata = (event: _StreamFilterOndataEvent) => {
+        const temp: Uint8Array = new Uint8Array(buffer.byteLength + event.data.byteLength);
+        temp.set(buffer);
+        temp.set(new Uint8Array(event.data), buffer.byteLength);
+        buffer = temp;
     }
 
-    return {cancel : isFiltered};
+    filter.onstop = () => {
+        const ignoringFilter : boolean = url.searchParams.get("skipfilter") !== null;
+        console.log(ignoringFilter);
+
+        isBlockedUrl(url.hostname, url)
+            .then((filteredURL: filterResults) => {
+                console.log(filteredURL);
+                if(filteredURL.blocked && !ignoringFilter) {
+                    filteredURL.url.searchParams.set("skipfilter", "true");
+                    filter.write(encoder.encode(
+                        `
+                        <body style="background-color: rgb(145,26,26)">
+                        <div style="text-align: center">
+                        <h1>The site ${filteredURL.sitename} is considered a dangerous website.</h1>
+                        <h2>It is considered a site that contains: ${filteredURL.tags}</h2>
+                        <a href="${filteredURL.url.href}" style="text-decoration: none; background-color: rgb(80,10,10); border-radius: 5px; color: black; padding: 5px">Proceed anyways</a>
+                        </div>
+                        </body>
+                        `
+                    ));
+                } else {
+                    filter.write(buffer);
+                }
+
+                filter.close();
+            });
+    };
+
+    return { cancel: false };
 }
 
 browser.webRequest.onBeforeRequest.addListener(handleRequest,{ urls: [ "<all_urls>" ], types: [ "main_frame" ] }, [ "blocking" ]);
