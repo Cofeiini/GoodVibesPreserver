e5ecb40325a6e4cffaafd61f53fea4f6fee5e98a
Cofeiini <cofeiini@gmail.com>
2023-10-21 22:33:34 +0300

    Add Optional wrapper class

---
+++ scripts/tools/optional.ts
@@ -0,0 +1,57 @@
+export class Optional<T> {
+    private data: Exclude<T, undefined> | null = null;
+
+    constructor(input: Exclude<T, undefined> | null) {
+        this.assign(input);
+    }
+
+    assign = (other: Exclude<T, undefined> | null) => {
+        if (other === undefined) {
+            throw new Error("Cannot assign undefined to Optional");
+        }
+
+        if (other === null) {
+            console.warn("Should not assign null to Optional. Use the reset member function instead");
+        }
+
+        this.data = other;
+    }
+
+    has_value = (): Boolean => {
+        return (this.data !== null && this.data !== undefined);
+    }
+
+    value = (): Exclude<T, undefined> => {
+        if (this.data !== null) {
+            return this.data;
+        }
+
+        throw new Error("Bad Optional access");
+    }
+
+    value_or = (default_value: T): T => {
+        if (this.data !== null) {
+            return this.data;
+        }
+
+        return default_value;
+    }
+
+    value_as = <U>(): Exclude<U, undefined> => {
+        if (this.data !== null) {
+            return this.data as Exclude<U, undefined>;
+        }
+
+        throw new Error("Bad Optional cast and access");
+    }
+
+    swap = (other: Optional<T>) => {
+        const temp = this.data;
+        this.data = other.data;
+        other.data = temp;
+    }
+
+    reset = () => {
+        this.data = null;
+    }
+}
