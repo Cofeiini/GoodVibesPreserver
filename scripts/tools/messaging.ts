61737b22807d5481f679f0a95090f5b8419e9895
Franco <francogargiulo2004@gmail.com>
2023-11-11 13:15:55 -0300

    Added messaging tool file containing interfaces and functions for the messaging system

---
+++ scripts/tools/messaging.ts
@@ -0,0 +1,53 @@
+import { urlFilter } from "./interfaces"
+
+export enum Action{
+    get_filters,
+    send_filters,
+    redirect,
+    add_listener,
+}
+
+export type message = {
+    /**
+     * @action 
+     * Represents the action that the message receiver is requested to do.
+     * 
+     * @data 
+     * Contains the message @content object.
+     * 
+     * @content 
+     * Contains any data that is sent through the message.
+     */
+    action : Action,
+    data : {
+        content: any
+    }
+}
+
+export class messagingMap extends Map<Action,Function> {
+
+    // Using same constructor as parent class.
+    constructor(entries : any)
+    {
+        super(entries)
+    }
+
+    /**
+     * @param
+     * key @action of the @message type represensting which function the custom map needs to return.
+     * 
+     * @returns 
+     * The function that matches the @param key, will return a void function with a debug message as fallback if key isn't found.
+     */
+    get(key : Action)
+    {
+        if(this.has(key))
+        {
+            return super.get(key) as Function;
+        }
+        
+        return () => { console.log("Bad Messaging Map access.")}
+    }
+}
+
+
