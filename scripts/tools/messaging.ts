f7461c0c814564034f543222d5e81ed5839f47f8
Franco <francogargiulo2004@gmail.com>
2023-11-19 11:22:10 -0300

    Revert "Fix warnings and cleanup code"
    This reverts commit f02d8dd788c65f862023805135bc87a4b6ddab6d.

--- scripts/tools/messaging.ts
+++ scripts/tools/messaging.ts
@@ -22,27 +22,31 @@ export type message = {
     data : {
         content: any
     }
-};
+}
 
-export class messagingMap extends Map<Action, Function> {
+export class messagingMap extends Map<Action,Function> {
 
     // Using same constructor as parent class.
-    constructor(entries : any) {
+    constructor(entries : any)
+    {
         super(entries)
     }
 
     /**
-     * @param key - The {@link Action} of the {@link message} type representing which function the custom map needs to return.
+     * @param
+     * key @action of the @message type represensting which function the custom map needs to return.
      * 
-     * @returns
-     * The function that matches the @param key. This will return a function with a debug message as fallback, if key isn't found.
+     * @returns 
+     * The function that matches the @param key, will return a void function with a debug message as fallback if key isn't found.
      */
-    get(key: Action) {
-        if(this.has(key)) {
+    get(key : Action)
+    {
+        if(this.has(key))
+        {
             return super.get(key) as Function;
         }
-
-        return () => { console.log("Bad Messaging Map access."); };
+        
+        return () => { console.log("Bad Messaging Map access.")}
     }
 }
 
