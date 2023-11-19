abb0a3286f039a7687cf76fc2e650ecabdecac31
Franco <francogargiulo2004@gmail.com>
2023-11-19 11:45:55 -0300

    Added analyze_element and removed add_listener actions.

--- scripts/tools/messaging.ts
+++ scripts/tools/messaging.ts
@@ -5,6 +5,7 @@ export enum Action{
     send_filters,
     redirect,
     add_listener,
+    analyze_elements,
 }
 
 export type message = {
@@ -33,9 +34,12 @@ export class messagingMap extends Map<Action,Function> {
     }
 
     /**
+     * @param key - The {@link Action} of the {@link message} type representing which function the custom map needs to return.
      * @param
      * key @action of the @message type represensting which function the custom map needs to return.
      * 
+     * @returns
+     * The function that matches the @param key. This will return a function with a debug message as fallback, if key isn't found.
      * @returns 
      * The function that matches the @param key, will return a void function with a debug message as fallback if key isn't found.
      */
