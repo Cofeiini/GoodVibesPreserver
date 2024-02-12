d567c64f516c318c7f0e9defe9e9263e0ab2f398
Franco <francogargiulo2004@gmail.com>
2024-02-12 18:57:01 -0300

    Add footer and header to query selectors

--- scripts/content/maxzindex.ts
+++ scripts/content/maxzindex.ts
@@ -1,6 +1,6 @@
 export let maxZIndex: number = 0;
 export const getMaxZIndex = (): void => {
-    maxZIndex = Math.max(...Array.from(document.querySelectorAll("body div, body img, body nav, body section"), (element) => {
+    maxZIndex = Math.max(...Array.from(document.querySelectorAll("footer, header, body div, body img, body nav, body section"), (element) => {
         return parseInt(getComputedStyle(element).zIndex);
     }).filter(zIndex => !Number.isNaN(zIndex)));
     maxZIndex++;
