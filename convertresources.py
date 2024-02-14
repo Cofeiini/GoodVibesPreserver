20b87b3646138e9dbbdbb2389b4321fe325270d4
Franco <francogargiulo2004@gmail.com>
2024-02-13 23:55:15 -0300

    Add script for converting .html and .css into .txt

---
+++ convertresources.py
@@ -0,0 +1,20 @@
+import os
+dir = "./htmlresources/"
+
+def convert():
+    for filename in os.listdir(dir):
+        filePath = os.path.join(dir, filename)
+        if os.path.isfile(filePath):
+            basename, extension = os.path.splitext(filename)
+            if extension.lower() in { ".css", ".html" }:
+                newFilename = os.path.join(dir, basename + ".txt")
+                fileData = open(filePath, "r").read()
+                if os.path.exists(newFilename):
+                    with open(newFilename, "w") as targetFile:
+                        targetFile.write(fileData)
+                else:
+                    with open(newFilename, "x") as newFile:
+                        newFile.write(fileData)
+
+convert()
+        
