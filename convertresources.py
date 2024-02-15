56f06c5d75495372926f43b42b995a70265404b6
Franco <francogargiulo2004@gmail.com>
2024-02-15 11:45:40 -0300

    Push missing files

--- convertresources.py
+++ convertresources.py
@@ -1,20 +1,22 @@
-import os
-dir = "./htmlresources/"
+if __name__ == '__main__':
 
-def convert():
-    for filename in os.listdir(dir):
-        filePath = os.path.join(dir, filename)
-        if os.path.isfile(filePath):
-            basename, extension = os.path.splitext(filename)
-            if extension.lower() in { ".css", ".html" }:
-                newFilename = os.path.join(dir, basename + ".txt")
-                fileData = open(filePath, "r").read()
-                if os.path.exists(newFilename):
-                    with open(newFilename, "w") as targetFile:
-                        targetFile.write(fileData)
-                else:
-                    with open(newFilename, "x") as newFile:
-                        newFile.write(fileData)
+    import os
+    dir = "./htmlresources/"
 
-convert()
+    def convert():
+        for filename in os.listdir(dir):
+            filePath = os.path.join(dir, filename)
+            if os.path.isfile(filePath):
+                basename, extension = os.path.splitext(filename)
+                if extension.lower() in { ".css", ".html" }:
+                    newFilename = os.path.join(dir, basename + ".txt")
+                    fileData = open(filePath, "r").read()
+                    if os.path.exists(newFilename):
+                        with open(newFilename, "w") as targetFile:
+                            targetFile.write(fileData)
+                    else:
+                        with open(newFilename, "x") as newFile:
+                            newFile.write(fileData)
+
+    convert()
         
