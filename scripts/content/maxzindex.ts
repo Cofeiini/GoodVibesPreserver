4e731af41e71cfc47bf23a18ea6c0f4075f42baf
Franco <francogargiulo2004@gmail.com>
2024-03-12 08:32:15 -0300

    Rename scripts folder to src

--- scripts/content/maxzindex.ts
+++
@@ -1,7 +0,0 @@
-export let maxZIndex: number = 0;
-export const getMaxZIndex = (): void => {
-    maxZIndex = Math.max(...Array.from(document.querySelectorAll("footer, header, body div, body img, body nav, body section"), (element) => {
-        return parseInt(getComputedStyle(element).zIndex);
-    }).filter(zIndex => !Number.isNaN(zIndex)));
-    maxZIndex++;
-};
