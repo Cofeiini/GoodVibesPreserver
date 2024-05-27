404006f8519a586b902a50b92d21e532aedb9627
Cofeiini <Cofeiini@gmail.com>
2024-05-27 16:26:43 +0300

    Fix eslint and whitelist thumbnails (#37)
    * Update eslint config to the new format
    * Fix some eslint warnings
    * Fix thumbnails that have no initial size
    * Update developer packages

--- src/background/makethumbnail.ts
+++ src/background/makethumbnail.ts
@@ -4,31 +4,31 @@ const maxHeight = 200;
 const makeThumbnail = (src: string) => {
     return new Promise((resolve, reject) => {
         const thumbnail = new Image();
-        thumbnail.src = src;
         thumbnail.onload = () => {
-            let width = thumbnail.width;
-            let height = thumbnail.height;
-            if (width > height) {
-                if (width > maxWidth) {
-                    height *= maxWidth / width;
-                    width = maxWidth;
-                }
-            } else {
-                if (height > maxHeight) {
-                    width *= maxHeight / height;
-                    height = maxHeight;
-                }
+            let width = thumbnail.width || maxWidth;
+            let height = thumbnail.height || maxHeight;
+
+            if ((width > height) && (width > maxWidth)) {
+                height *= maxWidth / width;
+                width = maxWidth;
             }
+            if ((height > width) && (height > maxHeight)) {
+                width *= maxHeight / height;
+                height = maxHeight;
+            }
+
             const canvas = document.createElement("canvas");
-            const context = canvas.getContext("2d");
             canvas.width = width;
             canvas.height = height;
+
+            const context = canvas.getContext("2d");
             context?.drawImage(thumbnail, 0, 0, width, height);
             resolve(canvas.toDataURL("image/jpeg"));
         };
         thumbnail.onerror = () => {
             reject("Error");
         };
+        thumbnail.src = src;
     });
 };
 
